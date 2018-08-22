4b0e3b055f4ac3349c10e85ef86c05e1763717d3
Olivier Willems <ojwillems@gmail.com>
2018-08-22 20:54:24 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -2,7 +2,6 @@ def my_select(collection)
   n = 0
   select_array = []
   while n < collection.size
-    select_array.push(yield collection[n])
     if yield(collection[n])
       select_array.push(collection[n])
     end
